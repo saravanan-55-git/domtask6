a65c67dc137480af45c33366eb8ff67f8e6a5a7e
saravanan-55-git <52695838+saravanan-55-git@users.noreply.github.com>
2022-10-12 12:28:26 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,14 @@
+function foo(){
+var firstname=document.getElementById("firstname").value;
+var lastname=document.getElementById("lastname").value;
+var middlename=document.getElementById("middlename").value;
+var email=document.getElementById("email").value;
+var pass=document.getElementById("pass").value;
+
+
+console.log(firstname);
+console.log(middlename);
+console.log(lastname);
+console.log(email);
+console.log(pass);
+    }
